a6155206ab854b5ee849035f5f93144779d4fd00
Archies Singh <18btrci004@jainuniversity.ac.in>
2021-04-18 10:05:43 +0530

    Project

--- Dance_Academy_Project/app.js
+++ Dance_Academy_Project/app.js
@@ -2,10 +2,29 @@ const http = require('http')
 const fs = require('fs')
 const express = require('express')
 const path = require("path")
-
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
 const app = express()
 
 const port = 8080;
+
+
+
+// Define Mongoose Schema
+mongoose.connect('mongodb://localhost/contactdance', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const contactschema = new mongoose.Schema({
+    name: String,
+    phone : String,
+    email: String,
+    address: String,
+    Comment: String
+  });
+
+const Contact = mongoose.model('Contact',  contactschema);
+
+
+
 
 // Adding Express directory
 app.use('/static',express.static('static'))// for serving the static folder
@@ -20,6 +39,20 @@ app.get('/',(req,res)=>{
     const params = {};
     res.render('home.pug',params)
 })
+
+app.get('/contact',(req,res)=>{
+    res.render('contact.pug')
+})
+
+app.post('/contact',(req,res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved ")
+    })
+})
+
 
 app.get('/contact',(req,res)=>{
     res.render('contact.pug')
